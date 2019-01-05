b4deb1aa611a4317d44622ce2e47d93a7a0372fa
skurtskurt <idriss.souissi12@gmail.com>
2019-01-05 18:39:33 +0100

    Try to request fixer.io API

--- Le_Balluchon/Le_Balluchon/Model/ExchangeRateService.swift
+++ Le_Balluchon/Le_Balluchon/Model/ExchangeRateService.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+class ExchangeRateService {
+    
+    var session: URLSession
+    
+    init(session: URLSession = URLSession(configuration: .default)) {
+        self.session = session
+    }
+    var task: URLSessionTask?
+    
+    func getRate(callback: @escaping (Bool, Double?) -> Void) {
+        task?.cancel()
+        guard let fixerUrl = URL(string: "http://data.fixer.io/api/latest?access_key=d0fdb72fd1a83139458ec4baca0ea2d1&symbols=USD") else {return}
+    
+        task = session.dataTask(with: fixerUrl, completionHandler: { (data, response, error) in
+                DispatchQueue.main.async {
+                print("OK")
+    
+                    guard let data = data, error == nil else {
+                        print("no data")
+                        callback(false, nil)
+                        return
+                    }
+        
+                    guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                        print("noresponse")
+                        callback(false, nil)
+                        return
+                    }
+                    
+                    guard let responseJSON = try? JSONDecoder().decode(ExchangeRate.self, from: data) else {
+                        callback(false, nil)
+                        return
+                    }
+                    
+                    let rate = responseJSON.rates["USD"]
+                    callback(true, rate)
+                }
+            
+        })
+        task?.resume()
+    }
+    
+}
+
