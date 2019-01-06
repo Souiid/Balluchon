9bc91fdf36142988aea3b2ade497cc5612df8c8e
skurtskurt <idriss.souissi12@gmail.com>
2019-01-06 19:57:20 +0100

    Add TranslationController

---
+++ Le_Balluchon/Le_Balluchon/Controller/TranslationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TranslationViewController.swift
+//  Le_Balluchon
+//
+//  Created by Souissi idriss2 on 06/01/2019.
+//  Copyright © 2019 Souissi idriss2. All rights reserved.
+//
+
+import UIKit
+
+class TranslationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
