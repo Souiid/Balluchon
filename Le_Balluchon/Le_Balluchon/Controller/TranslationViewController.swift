b9c811299b7903b9194e428b2ff7a4212ef39093
skurtskurt <idriss.souissi12@gmail.com>
2019-01-06 19:57:50 +0100

    Add outlets

--- Le_Balluchon/Le_Balluchon/Controller/TranslationViewController.swift
+++ Le_Balluchon/Le_Balluchon/Controller/TranslationViewController.swift
@@ -16,15 +16,10 @@ class TranslationViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var translateTextView: UITextView!
+    
+    @IBOutlet weak var translateButton: UIButton!
+    
 
 }
