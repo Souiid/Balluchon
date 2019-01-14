959bd1e2520b9d32a12f08cce489d70ed3239398
skurtskurt <idriss.souissi12@gmail.com>
2019-01-14 19:08:51 +0100

    Add WeatherViewController

---
+++ Le_Balluchon/Le_Balluchon/Model/TranslationDecodable.swift
@@ -0,0 +1,9 @@
+//
+//  TranslationDecodable.swift
+//  Le_Balluchon
+//
+//  Created by Souissi idriss2 on 07/01/2019.
+//  Copyright © 2019 Souissi idriss2. All rights reserved.
+//
+
+import Foundation
