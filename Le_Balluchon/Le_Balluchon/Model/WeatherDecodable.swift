4219ef550fa2429bb466ccf86a3ef69dbac5d611
skurtskurt <idriss.souissi12@gmail.com>
2019-02-04 18:04:35 +0100

    Add Decodable

--- Le_Balluchon/Le_Balluchon/Model/WeatherDecodable.swift
+++ Le_Balluchon/Le_Balluchon/Model/WeatherDecodable.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2019 Souissi idriss2. All rights reserved.
 //
 
+
+
+// To parse the JSON, add this file to your project and do:
+//
+//   let welcome = try? newJSONDecoder().decode(Welcome.self, from: jsonData)
+
+
+
+// To parse the JSON, add this file to your project and do:
+//
+//   let welcome = try? newJSONDecoder().decode(Welcome.self, from: jsonData)
+
 import Foundation
+
+
+struct WeatherDecodable: Decodable {
+    let cnt: Int
+    let list: [List]
+}
+
+struct List: Decodable {
+    let weather: [Weather]
+    let main: Main
+    let wind: Wind
+    let clouds: Clouds
+    let name: String
+}
+
+struct Clouds: Decodable {
+    let all: Int
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let id: Int
+    let main, description: String
+}
+
+struct Wind: Decodable {
+    let speed, deg: Double
+}
+
+
+
