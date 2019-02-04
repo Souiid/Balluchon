44edd0704a77d1a77028a28b1d2c12d566afeabf
skurtskurt <idriss.souissi12@gmail.com>
2019-02-04 18:05:11 +0100

    Complete call to the API

--- Le_Balluchon/Le_Balluchon/Model/WeatherService.swift
+++ Le_Balluchon/Le_Balluchon/Model/WeatherService.swift
@@ -4,13 +4,13 @@
 //
 //  Created by Souissi idriss2 on 14/01/2019.
 //  Copyright © 2019 Souissi idriss2. All rights reserved.
+
+//Label Meteo, Gestion clavier, containte chiffre rate, url translate, commentaires
 //
 
 import Foundation
 
 class WeatherService {
-    
- let weatherURL = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=c0c3352eed0b11d7ee13876326e5a710")!
     
     var session: URLSession
     init(session: URLSession = URLSession(configuration: .default)) {
@@ -19,10 +19,10 @@ class WeatherService {
     
     var task: URLSessionTask?
     
-    
-    func getWeather(callback: @escaping (Bool, Double?) -> Void) {
+    func getWeather(callback: @escaping (Bool, WeatherDecodable?) -> Void) {
         task?.cancel()
-       
+        //Déballer
+       let weatherURL = URL(string: "http://api.openweathermap.org/data/2.5/group?id=5128638,2983154&units=metric&appid=c0c3352eed0b11d7ee13876326e5a710")!
         
         task = session.dataTask(with: weatherURL, completionHandler: { (data, response, error) in
             DispatchQueue.main.async {
@@ -40,14 +40,17 @@ class WeatherService {
                     return
                 }
                 
-//                guard let responseJSON = try? JSONDecoder().decode(ExchangeRate.self, from: data) else {
-//                    callback(false, nil)
-//                    return
-//                }
-                print("DATA : ", data)
+                guard let responseJSON = try? JSONDecoder().decode(WeatherDecodable.self, from: data) else {
+                    callback(false, nil)
+                    print("NO ResponseJson")
+                    return
+                }
+                callback(true, responseJSON)
             }
             
         })
         task?.resume()
     }
+    
+   
 }
