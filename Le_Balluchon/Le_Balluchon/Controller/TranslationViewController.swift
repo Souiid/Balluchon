cc81600b78d17248f3a9a05d3627177c3765d400
skurtskurt <idriss.souissi12@gmail.com>
2019-02-04 18:02:59 +0100

    Change labels language accordding to segmentControl

--- Le_Balluchon/Le_Balluchon/Controller/TranslationViewController.swift
+++ Le_Balluchon/Le_Balluchon/Controller/TranslationViewController.swift
@@ -9,26 +9,51 @@
 import UIKit
 
 class TranslationViewController: UIViewController {
-
-    var translationService = TranslationService()
+//Renommer outlets
+    let translationService = TranslationService()
     @IBOutlet weak var inputTextField: UITextField!
+    
+    @IBOutlet weak var controllerTitleLabel: UILabel!
+    
+    @IBOutlet weak var langageSegmentControl: UISegmentedControl!
     @IBOutlet weak var translateTextView: UITextView!
     @IBOutlet weak var translateButton: UIButton!
     
-    var textToTranslate = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        langageSegmentControl.selectedSegmentIndex = 0
+        changeLanguage(langageSegmentControl)
     }
     
    @IBAction func displayTranslation() {
         guard let textToTranslate = inputTextField.text else {return}
-        translationService.getTranslation(text: textToTranslate) { (success, textTranslated) in
+        translationService.getTranslation(selectedIndex: langageSegmentControl.selectedSegmentIndex, text: textToTranslate) { (success, textTranslated) in
             guard let textTranslated = textTranslated else {return}
             self.translateTextView.text = textTranslated
         }
     }
+    
 
+    @IBAction func changeLanguage(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            controllerTitleLabel.text = "Translation"
+            translateButton.setTitle("Translate", for: .normal)
+            translateTextView.text = "Translate"
+        case 1:
+            controllerTitleLabel.text = "Traduction"
+            translateButton.setTitle("Traduire", for: .normal)
+            translateTextView.text = "Traduction"
+        default:
+            return
+        }
+        
+    }
+    
+    @IBAction func dismissKeybord(_ sender: UITapGestureRecognizer) {
+        inputTextField.resignFirstResponder()
+    }
+    
 }
