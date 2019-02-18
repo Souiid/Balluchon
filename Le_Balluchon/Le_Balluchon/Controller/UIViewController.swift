4ead7ec399f13552495382c86f4d7eb1001e9228
skurtskurt <idriss.souissi12@gmail.com>
2019-02-18 19:34:08 +0100

    Add extension to display alert

--- Le_Balluchon/Le_Balluchon/Controller/UIViewController.swift
+++ Le_Balluchon/Le_Balluchon/Controller/UIViewController.swift
@@ -8,23 +8,15 @@
 
 import UIKit
 
-class UIViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    extension UIViewController {
+        
+        func presentAlert(message: String) {
+            let alertVc = UIAlertController(title: "Zéro", message: message, preferredStyle: .alert)
+            alertVc.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(alertVc, animated: true, completion: nil)
+        }
     }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
